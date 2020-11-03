787e09fa89723ef6db9684a14cd4f17373d33203
unknown <hsc.jr94@gmail.com>
2020-11-03 09:39:44 -0300

    frist commit

---
+++ app.js
@@ -0,0 +1,99 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//BD connect with mongoose
+mongoose.connect("mongodb+srv://admin:4rg4m4ss4@cluster0.x7bqk.mongodb.net/todolistDB?retryWrites=true&w=majority",{useNewUrlParser:true});
+
+//Schema
+const itemsSchema = {
+  name: {
+    type: String,
+    required: true
+  }
+};
+
+
+//new model
+const Item = mongoose.model("Item",itemsSchema);
+
+//function save
+function saveItem(item){
+  const data = new Item ({
+    name: item
+  });
+  data.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+
+    }
+
+
+  });
+}
+
+const workItems = [];
+
+
+
+
+
+app.get("/", function(req, res) {
+  const day = date.getDate();
+  Item.find({},function(err, foundItems){
+
+    res.render("list", {listTitle: day, newListItems: foundItems});
+  });
+
+
+});
+
+//submit button action
+app.post("/", function(req, res){
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    saveItem(item);
+    res.redirect("/work");
+  } else {
+    saveItem(item);
+    res.redirect("/");
+  }
+});
+
+//delete button
+app.post("/delete",function(req,res){
+  const checkedId = req.body.checkbox;
+  Item.remove({_id:checkedId},function(err){
+    if(err){
+      console.log(err);
+    }
+  });
+
+
+  res.redirect("/");
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
